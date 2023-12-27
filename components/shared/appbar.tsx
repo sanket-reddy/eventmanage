760f7d82bc16142fbb902ed19efa18360828fcca
postittom3@gmail.com <pathuri.sanketh@gmail.com>
2023-12-28 03:08:03 +0530

    nav bar css done

--- components/shared/appbar.tsx
+++ components/shared/appbar.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { Button } from "../ui/button";
 import Link from "next/link";
+import Image from "next/image";
+import { Separator } from "../ui/separator";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 const Appbar = () => {
   return (
     <div>
-      <nav className="flex w-full p-3 bg-violet-200 justify-between items-center ">
+      <nav className="flex w-full p-5 bg-violet-200 justify-between items-center ">
         <div>
           <h1 className="text-2xl">SITE-1</h1>
         </div>
-        <div className="flex space-x-4 ">
+        <div className=" hidden lg:flex space-x-4 ">
           <div className=" hover:text-blue-800">
             <Link href="/home">Home</Link>
           </div>
@@ -20,12 +30,38 @@ const Appbar = () => {
             <Link href="/More">MORE </Link>
           </div>
         </div>
-        <Button
-          variant="secondary"
-          className="bg-blue-500 rounded-full mr-3 hover:bg-cyan-400"
-        >
-          click here
-        </Button>
+        <div className="flex justify-between items-center">
+          <Button variant="default" className="mt-0 bg-blue-800 px-7">
+            Login
+          </Button>
+          <Sheet>
+            <SheetTrigger className="mx-4 sm:hidden">
+              <Image
+                src="/menu.png"
+                alt="menu logo"
+                width={40}
+                height={60}
+              ></Image>
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle className="text-3xl">Evently</SheetTitle>
+                <Separator />
+                <SheetDescription className="flex flex-col space-y-3 ">
+                  <Link href="/Home" className="text-lg text-black">
+                    Home
+                  </Link>
+                  <Link href="/About" className="text-lg text-black">
+                    About
+                  </Link>
+                  <Link href="/More" className="text-lg text-black">
+                    More
+                  </Link>
+                </SheetDescription>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+        </div>
       </nav>
     </div>
   );
